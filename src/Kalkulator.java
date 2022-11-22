f2293b4331c9e6ab6a83c03e65dc42ec4b262d08
klaudia-sek <klaudia.anna.sek@gmail.com>
2022-11-22 20:52:58 +0100

    Prosty kalkulator dodawanie

---
+++ src/Kalkulator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Kalkulator {
+    public static void main(String[] args) {
+        System.out.println("Podaj pierwszą cyfrę");
+        Scanner scanner=new Scanner(System.in);
+        int number1= scanner.nextInt();
+        System.out.println("Podaj drugą cyfrę");
+        int number2= scanner.nextInt();
+        System.out.println("Suma "+(number1%number2));
+    }
+}
